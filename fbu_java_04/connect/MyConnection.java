4a898473a2aabf4f07dfc2978b9b08dfc9ffbf06
huyhv68 <156073915+huyhv68@users.noreply.github.com>
2024-01-09 17:01:13 +0700

    Add files via upload

---
+++ fbu_java_04/connect/MyConnection.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.cdbk.bgb.fbu_java_04.connect;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author user
+ */
+public class MyConnection {
+
+    private String dbURL = "jdbc:mysql://localhost:3306/fbu_java_04";
+    private String userName = "root";
+    private String password = "";
+    private Connection conn = null;
+
+    public Connection getConnection() {
+        try {
+            if (conn == null) {
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                conn = DriverManager.getConnection(dbURL, userName, password);
+            }
+        } catch (Exception e) {
+        }
+        return conn;
+    }
+}
